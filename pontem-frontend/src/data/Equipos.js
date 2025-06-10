bea11d04f836b288bf3036a43ea4627c3a028ab9
EugenioCruz <ecruz3@uc.cl>
2025-06-09 23:19:45 -0400

    Update equipos

--- pontem-frontend/src/data/Equipos.js
+++ pontem-frontend/src/data/Equipos.js
@@ -10,44 +10,45 @@ const teamData = [
     id: 2,
     name: 'Relaciones Externas',
     members: ['Nicolás Herrera', 'Catalina Guzmán', 'Bernardita Castellón', 'Pedro Munita'],
-    role: 'Encargado de la relación con Consultoras y otras organizaciones, estableciendo y manteniendo alianzas estratégicas que beneficien al club y sus miembros.',
+    role: 'Gestionar las relaciones con instituciones de la UC, tales como centros de estudiantes, FEUC, proyectos universitarios, etc. También se deberá gestionar las relaciones con clubes y organizaciones a nivel nacional e internacional, con tal de establecer benchmarks para la proyección del Club. Desarrollar redes de networking, conectar a Pontem con la realidad externa y garantizar la adaptación a su contexto.',
     imageUrl: 'equipos/RelacionesExternas.JPG',
   },
   {
     id: 3,
     name: 'Gestión de Personas',
     members: ['Gaspar Villarroel', 'Bernardita Castellón', 'Felipe Besnier'],
-    role: 'Encargado de la gestión de personas y bienestar laboral, asegurando un ambiente de trabajo positivo y promoviendo el desarrollo profesional de los miembros del club.',
+    role: 'Diseñar y desarrollar el proceso de reclutamiento, garantizar la atracción del talento y la interdisciplinariedad. Generar comunidad y mantener buenas relaciones entre miembros, gestionar el desempeño y promover el crecimiento del capital humano. Velar por el alineamiento estratégico de las distintas áreas y generar sentido de pertenencia en el Club.',
     imageUrl: 'equipos/GDP.JPG',
   },
   {
     id: 4,
     name: 'Comunicaciones & Marketing',
     members: ['Consuelo Moreno', 'Santiago Ernst', 'María Ignacia Meza'],
-    role: 'Encargado de estrategias de comunicación y redes sociales, gestionando la imagen pública del club y promoviendo sus actividades a través de diversos canales de comunicación.',
+    role: 'Comunicar la misión, objetivos y actividades del Club. Desarrollar contenido y ser el soporte transversal para todas las áreas del Club. Posiciona al Club como una organización de excelencia, teniendo un rol fundamental en la difusión de los proyectos de Pontem, penetración en la comunidad UC y en registrar el impacto de nuestras iniciativas.',
     imageUrl: 'equipos/C&M.JPG',
   },
   {
     id: 5,
     name: 'Learning & Development',
     members: ['Joaquín Bustamante', 'Nicolás Prohens', 'Pedro Sanzana', 'Romina Bakulic', 'Vicente Álvarez', 'Francisco Allende'],
-    role: 'Encargado de la capacitación y desarrollo de los miembros del club, organizando talleres, cursos y otras actividades formativas que contribuyan al crecimiento profesional de los integrantes.',
+    role: 'Apoyar a los miembros del Club y la comunidad UC a explorar el mundo de la consultoría y preparar los procesos de recruiting de las diferentes consultoras mediante elaboración y recopilación de material de estudio y la realización de actividades con consultoras. Trabajar de manera proactiva y en equipo para poder entregar un abanico de proyectos de alto impacto.',
     imageUrl: 'equipos/L&D.JPG',
   },
   {
     id: 6,
     name: 'Consultoría Social',
     members: ['Matías Campos', 'Antonio López', 'José Tomás Carvajal', 'Sebastián Vigueras'],
-    role: 'Encargado de la realización de proyectos de consultoría social, trabajando en iniciativas que generen un impacto positivo en la comunidad y fomenten la responsabilidad social del club.',
+    role: 'Entregar apoyo de calidad y bajo costo a aquellas instituciones o empresas que desean impulsar el bienestar social desde sus organizaciones, que no cuentan con la capacidad financiera para contratar un servicio de consultoría. Buscar activamente estas organizaciones, liderar equipos interdisciplinarios, gestionar el desarrollo de los proyectos de consultoría y garantizar un servicio de calidad.',
     imageUrl: 'equipos/CS.JPG',
   },
   {
     id: 7,
     name: 'Finanzas',
     members: ['Maximiliano Riveros', 'Andrés Molina'],
-    role: 'Administración de presupuesto y finanzas del club, gestionando los recursos económicos de manera eficiente y asegurando la sostenibilidad financiera de todas las actividades del club.',
+    role: 'Gestionar ingresos, gastos y presupuestos del club, explorando nuevas fuentes de financiamiento para cada una de las áreas y proyectos. Encargada de apoyar a otras áreas en proyectos inter-coordinaciones. Responsable de avances en materia de constitución legal de Pontem.',
     imageUrl: 'equipos/Finanzas.JPG',
   }
+  // TODO: Agregar equipo de TI (Tecnología e Información)
   ];
 
     export default teamData;
